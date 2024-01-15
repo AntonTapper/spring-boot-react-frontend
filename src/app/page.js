e67f936e5c4ea99e2da1ead4da7786a83d9a0136
Anton Tapper Dock <at@appsson.se>
2024-01-15 22:04:26 +0100

    started with homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 "use client"
 import Image from 'next/image'
 import MyComponent from '../components/MyComponent'
+import HomePage from '@/containers/HomePage/HomePage'
 
 export default function Home() {
-  return MyComponent();
-  
+  return HomePage()
     
 }
---
+++ src/containers/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+        {/* <HotelSlider/> */}
+        <section className='welcome-section'>
+            <h2>Welcome to My Hotel Booking App</h2>
+            <p>Discover hotels all around the world!</p>
+        </section>
+    </div>
+  )
+}
+
+export default HomePage
